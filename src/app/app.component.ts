72b058af985f22c3ff47906b82a893abbca14e0c
Viktor Säubert <vsaeubert@gmail.com>
2022-03-06 16:48:23 +0100

    Added img and started...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'viktorsaeubert';
+export class AppComponent implements OnInit {
+
+  images = ['desk.jpg', 'forest.jpg', 'office.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+
+  ngOnInit(): void {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 5);
+
+    }, 6000);
+  }
+
 }
