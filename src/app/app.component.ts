3f52c6fc63407ee6227a7e8ae547507dd2bad74d
Viktor Säubert <vsaeubert@gmail.com>
2022-03-08 15:52:06 +0100

    started header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,28 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  images = ['desk.jpg', 'forest.jpg', 'office.jpg'];
-  currentImage = 0;
-  showImage = true;
+export class AppComponent {
 
 
-  ngOnInit(): void {
-    this.updateImage();
-  }
-
-  updateImage() {
-
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-      setTimeout(() => {
-        this.showImage = true;
-      }, 5);
-
-    }, 6000);
-  }
 
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+// All from selector can be included in main HTML
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+
+  // implement OnInit means: this script is performing at onload
+  constructor() { }
+
+  images = ['desk.jpg', 'forest.jpg', 'office.jpg'];
+  headlines = [
+    'Bring engineering to the next level',
+    'Passionate web developement',
+    'Most happy users'];
+  currentImage = 0;
+  showImage = true;
+
+// Function on onload. The name is set by default
+  ngOnInit(): void {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 5);
+
+    }, 5000);
+  }
+
+}
