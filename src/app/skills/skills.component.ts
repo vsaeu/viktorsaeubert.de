c99afbf720207b73f36ea62a9871d968e83b62fb
Viktor Säubert <vsaeubert@gmail.com>
2022-04-03 22:17:45 +0200

    My skills with ngFor

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -9,7 +9,45 @@ export class SkillsComponent implements OnInit {
 
   constructor() { }
 
+  skills =
+    [
+      {
+        name: 'Javascript',
+        path: "angular.svg"
+      },
+
+
+      {
+        name: 'Angular',
+        path: "angular.svg"
+      }
+
+    ]
+  //  "HTML / CSS":"CSS3.png",
+  //  "SCRUM":"Scrum.png",
+  //  "Git":"Git.png",
+  //  "Design Thinking":"DesignThinking.png",
+  //  "Rest API":"API.png",
+  //  "Test automation":"testAutomation.png",
+  //  "Databases":"database.png"
+
+
+
+
+
+
+  skill: string[] = [];
+
+
+
+  //  kill = this.skills['Javascript'][0];
+
   ngOnInit(): void {
+    console.log(this.skills[0]['path'])
+    // json.skills.array.forEach(element => {
+    //   skill.push(element.name)
+    // });
+
   }
 
 }
